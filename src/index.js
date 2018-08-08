db8dbebeb61062dd98e8ca564939063dea5632b7
Дмитрий Рог <dmitry@pc.local>
2018-08-08 21:56:27 +0400

    Ejected

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import App from './components'
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker()
